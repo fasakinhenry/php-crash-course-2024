4e92e4a256236a7282c1008535dbec44a1752be4
Fasakin Henry <fasakinhenry@gmail.com>
2024-02-14 20:40:05 +0100

    Add code to master PHP syntax

--- 00_syntax.php
+++ 00_syntax.php
@@ -8,7 +8,17 @@
   <title>Document</title>
 </head>
 <body>
-Hello World
-<!--Echo HTML tags-->
+	<?php
+		echo "Hello world";
+		echo " and PHP";
+	?>
+
+	<?php
+		echo '<br>';
+		echo '<button>Click me</button> ';
+		echo "Hello world from another tag";
+	?>
 </body>
 </html>
+
+
